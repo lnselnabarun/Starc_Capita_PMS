f3445d75e897a680c81441a7c69cd273a8b0a20b
nabarun1995 <nabarun.adhikary95@gmail.com>
2024-09-06 15:16:47 +0530

    update

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constant";
 import Logo from "./common/logo";
 
-const Navbar = ({onpress}) => {
+const Navbar = ({ onpress }) => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="w-full flex py-0 justify-between items-center">
-     <Logo/>
+      <Logo />
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
@@ -39,7 +39,8 @@ const Navbar = ({onpress}) => {
       >
         <ul className="list-none flex flex-col justify-end items-center flex-1">
           {navLinks.map((nav, index) => (
-            <li onClick={() => setToggle(false)}
+            <li
+              onClick={() => setToggle(false)}
               key={nav.id}
               className={` text-neutral-900 font-poppins font-medium  cursor-pointer text[16px] ${
                 index === navLinks.length - 1 ? "mb-0" : "mb-4"
@@ -51,23 +52,23 @@ const Navbar = ({onpress}) => {
         </ul>
       </div>
       <div className=" justify-end  z-10 items-end flex mr-1 ml-5 ">
-          <button
-            onClick={onpress}
-            type="button"
-            className={`py-2 px-6 font-poppins font-semibold text-[15px] text-primary outline-none bg-fuchsia-900 rounded-full flex hover:bg-fuchsia-700 transition-colors`}
-          >
-            LOGIN
-          </button>
-        </div>
-        <div className=" justify-end  z-10 items-end flex ml-5 mr-5 ">
-          <button
-            onClick={() => {}}
-            type="button"
-            className={`py-2 px-6 font-poppins font-semibold text-[15px] text-primary outline-none bg-fuchsia-900 rounded-full flex hover:bg-fuchsia-700 transition-colors`}
-          >
-            SIGN UP
-          </button>
-        </div>
+        <button
+          onClick={onpress}
+          type="button"
+          className={`py-2 px-6 font-poppins font-semibold text-[15px] text-primary outline-none bg-fuchsia-900 rounded-full flex hover:bg-fuchsia-700 transition-colors`}
+        >
+          LOGIN
+        </button>
+      </div>
+      <div className=" justify-end  z-10 items-end flex ml-5 mr-5 ">
+        <button
+          onClick={() => {}}
+          type="button"
+          className={`py-2 px-6 font-poppins font-semibold text-[15px] text-primary outline-none bg-fuchsia-900 rounded-full flex hover:bg-fuchsia-700 transition-colors`}
+        >
+          SIGN UP
+        </button>
+      </div>
     </nav>
   );
 };
