e3b05537902e1c00940a13546accb89fe95fb15c
nabarun1995 <nabarun.adhikary95@gmail.com>
2024-09-13 11:55:20 +0530

    update

--- app/Dashboard/page.jsx
+++ app/Dashboard/page.jsx
@@ -71,25 +71,28 @@ export default function Dashboard() {
 
   const tableData = [
     {
-      currency: "Bitcoin",
+      currency: "Bitcoin / BTC",
       price: "$50,000",
       cagr: "2%",
       statistic: require("../assets/logo/Graph.png"),
       exchange: "Binance",
+      coin: require("../assets/logo/Bitcoin.png"),
     },
     {
-      currency: "Ethereum",
+      currency: "Ethereum / ETH",
       price: "$4,000",
       cagr: "3%",
       statistic: require("../assets/logo/Graph1.png"),
       exchange: "Coinbase",
+      coin: require("../assets/logo/Icon1.png"),
     },
     {
-      currency: "Ripple",
+      currency: "Algorand / ALG",
       price: "$1.50",
       cagr: "1.5%",
       statistic: require("../assets/logo/Graph2.png"),
       exchange: "Kraken",
+      coin: require("../assets/logo/Icon2.png"),
     },
   ];
   const menuItemForHome = ["Dashboard", "Analysis"];
@@ -204,7 +207,7 @@ export default function Dashboard() {
         ) : null}
 
         {activeIndex === 0 && activeIndexSecond === 0 ? (
-          <div className="flex flex-col md:flex-row w-full justify-between px-4 md:px-28 lg:px-28">
+          <div className="flex flex-col md:flex-row w-full justify-between px-4 sm:px-6 lg:px-28">
             {/* First div with 70% width on medium and larger screens */}
             <div className="w-full md:w-[70%] flex flex-wrap gap-4 justify-between">
               {/* Card 1 */}
@@ -450,10 +453,13 @@ export default function Dashboard() {
               <div className="container mx-auto px-4 py-6">
                 {/* Table Header */}
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 text-left p-3 bg-[#F5F5F5] rounded-lg">
-                  <div className="text-xs sm:text-xs md:text-xs font-normal leading-6 text-left text-[#848CA9]">
+                  <div className="text-xs sm:text-xs md:text-xs font-normal leading-6 text-left text-[#848CA9] ">
+                    {/* Added padding-right */}
                     CURRENCY NAME
                   </div>
-                  <div className="text-xs sm:text-xs md:text-xs font-normal leading-6 text-left text-[#848CA9]">
+                  <div className="text-xs sm:text-xs md:text-xs font-normal leading-6 text-left text-[#848CA9] ml-4">
+                    {" "}
+                    {/* Added padding-right */}
                     PRICE
                   </div>
                   <div className="text-xs sm:text-xs md:text-xs font-normal leading-6 text-left text-[#848CA9]">
@@ -473,10 +479,23 @@ export default function Dashboard() {
                     key={index}
                     className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 bg-[#F5F5F5] text-left p-2 my-2 justify-center items-center rounded-lg"
                   >
-                    <div className="font-poppins text-base sm:text-sm md:text-base font-medium leading-6 text-left text-[#3F4765]">
-                      {row.currency}
+                    <div className="flex items-center space-x-4">
+                      {" "}
+                      {/* Increased space between image and currency */}
+                      <Image
+                        src={row?.coin}
+                        alt="Currency Image"
+                        width={32}
+                        height={32}
+                        className="h-8 w-8"
+                      />
+                      <div className="font-poppins text-base sm:text-sm md:text-base font-medium leading-6 text-left text-[#3F4765]">
+                        {row.currency}
+                      </div>
                     </div>
-                    <div className="font-poppins text-base sm:text-sm md:text-base font-medium leading-6 text-left text-[#3F4765]">
+                    <div className="font-poppins text-base sm:text-sm md:text-base font-medium leading-6 text-left text-[#3F4765] pl-4">
+                      {" "}
+                      {/* Added padding to price column */}
                       {row.price}
                     </div>
                     <div className="font-poppins text-base sm:text-sm md:text-base font-medium leading-6 text-left text-[#3F4765]">
@@ -500,7 +519,253 @@ export default function Dashboard() {
             </div>
 
             {/* Second div with 20% width on medium and larger screens */}
-            <div className="w-full md:w-[24%] bg-green-300 p-4"></div>
+            <div className="w-full md:w-[27%] bg-[#f5F5F5F5] p-1 sm:p-2">
+              <div className="flex items-center space-x-4 justify-between">
+                <div className="font-sans text-lg sm:text-base md:text-lg font-medium leading-5 text-left text-[#3F4765]">
+                  My Balance
+                </div>
+                <div className="flex items-center justify-center bg-white rounded-full h-8 w-8 sm:h-10 sm:w-10">
+                  <Image
+                    src={require("../assets/logo/IconPlus.png")}
+                    alt="Centered Image"
+                    width={20}
+                    height={20}
+                    className="h-5 w-5"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col sm:flex-row justify-between space-x-0 sm:space-x-4 mt-3">
+                {/* <!-- First Card --> */}
+                <div className="relative bg-[#60BC63] rounded-lg p-1 sm:p-2 w-full sm:w-[48%] h-auto">
+                  {/* <!-- First Two Text Elements in Column --> */}
+                  <div className="flex flex-col space-y-2">
+                    <div className="text-white font-semibold text-base sm:text-sm md:text-base">
+                      US Dollar
+                    </div>
+                    <div className="text-white font-semibold text-base sm:text-sm md:text-base">
+                      80,435.712
+                    </div>
+                  </div>
+
+                  {/* <!-- Dashed Border --> */}
+                  <div className="border-t border-dashed border-white my-4"></div>
+
+                  {/* <!-- Third Text and Image in Column --> */}
+                  <div className="flex flex-col space-y-2">
+                    <div className="text-white font-semibold text-base sm:text-sm md:text-base">
+                      {`$ 0,0014`}
+                    </div>
+                    <div className="w-6 h-6 sm:w-8 sm:h-8">
+                      <Image
+                        src={require("../assets/logo/Credit_Card_green.png")}
+                        alt="Card Image"
+                        width={24}
+                        height={24}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  </div>
+
+                  {/* <!-- Absolute Image at Bottom Right --> */}
+                  <div className="absolute bottom-0 right-0 w-15 h-8 sm:w-10 sm:h-10">
+                    <Image
+                      src={require("../assets/logo/Highlight_green.png")}
+                      alt="Value Image"
+                      width={100}
+                      height={100}
+                      objectFit="contain"
+                      className="w-full h-full"
+                    />
+                  </div>
+                </div>
+
+                {/* <!-- Second Card --> */}
+                <div className="relative bg-[#FFBA33] rounded-lg p-1 sm:p-2 w-full sm:w-[48%] h-auto">
+                  {/* <!-- First Two Text Elements in Column --> */}
+                  <div className="flex flex-col space-y-2">
+                    <div className="text-white font-semibold text-base sm:text-sm md:text-base">
+                      Bitcoin
+                    </div>
+                    <div className="text-white font-semibold text-base sm:text-sm md:text-base">
+                      1.84333767
+                    </div>
+                  </div>
+
+                  {/* <!-- Dashed Border --> */}
+                  <div className="border-t border-dashed border-white my-4"></div>
+
+                  {/* <!-- Third Text and Image in Column --> */}
+                  <div className="flex flex-col space-y-2">
+                    <div className="text-white font-semibold text-base sm:text-sm md:text-base">
+                      {`$ 109106,60`}
+                    </div>
+                    <div className="w-6 h-6 sm:w-8 sm:h-8">
+                      <Image
+                        src={require("../assets/logo/Credit_Card_yellow.png")}
+                        alt="Card Image"
+                        width={24}
+                        height={24}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  </div>
+
+                  {/* <!-- Absolute Image at Bottom Right --> */}
+                  <div className="absolute bottom-0 right-0 w-15 h-8 sm:w-10 sm:h-10">
+                    <Image
+                      src={require("../assets/logo/Highlight_yellow.png")}
+                      alt="Value Image"
+                      width={100}
+                      height={100}
+                      objectFit="contain"
+                      className="w-full h-full"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex space-x-4 mt-4">
+                <div className="w-1/2">
+                  <Image
+                    src={require("../assets/logo/Withdraw.png")} // replace with your image path
+                    alt="Image 1"
+                    width={500} // adjust the width as needed
+                    height={300} // adjust the height as needed
+                    className="w-full h-auto object-cover"
+                  />
+                </div>
+                <div className="w-1/2 ">
+                  <Image
+                    src={require("../assets/logo/Deposit.png")} // replace with your image path
+                    alt="Image 2"
+                    width={500} // adjust the width as needed
+                    height={300} // adjust the height as needed
+                    className="w-full h-auto object-cover"
+                  />
+                </div>
+              </div>
+              <div className="flex items-center space-x-4 justify-between mt-2">
+                <div className="font-sans text-lg sm:text-base md:text-lg font-medium leading-5 text-left text-[#3F4765]">
+                  Activities
+                </div>
+                <div className="font-sans text-sm sm:text-base md:text-sm font-medium leading-5 text-left text-[#969CCB]">
+                  {`Today  ▼`}
+                </div>
+              </div>
+
+              <div className="flex justify-between items-start space-y-4 md:space-y-0 flex-row mt-2">
+                {/* First Section: Image and Text in one row */}
+                <div className="flex items-center space-x-4">
+                  <Image
+                    src={require("../assets/logo/Logo_Bit.png")}
+                    alt="Currency Image"
+                    width={46}
+                    height={46}
+                  />
+                  <div className="flex flex-col space-y-2">
+                    <p className="text-xs font-semibold text-[#3F4765] ">
+                      Bitcoin{" "}
+                    </p>
+                    <p className="text-sm font-normal text-[#3F4765]">Sell </p>
+                  </div>
+                </div>
+
+                {/* Second Section: Two text elements column-wise */}
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-semibold text-[#F85842] text-end">
+                    $2,435.80{" "}
+                  </p>
+                  <p className="text-xs font-normal text-[#3F4765] text-end">
+                    Today | 16.40
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex justify-between items-start space-y-4 md:space-y-0 flex-row mt-4">
+                {/* First Section: Image and Text in one row */}
+                <div className="flex items-center space-x-4">
+                  <Image
+                    src={require("../assets/logo/Logo_B.png")}
+                    alt="Currency Image"
+                    width={46}
+                    height={46}
+                  />
+                  <div className="flex flex-col space-y-2">
+                    <p className="text-xs font-semibold text-[#3F4765] ">
+                      Ethereum{" "}
+                    </p>
+                    <p className="text-sm font-normal text-[#3F4765]">Buy </p>
+                  </div>
+                </div>
+
+                {/* Second Section: Two text elements column-wise */}
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-semibold text-[#24A959] text-end">
+                    $1,435.72
+                  </p>
+                  <p className="text-xs font-normal text-[#3F4765] text-end">
+                    Today | 16.40
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-between items-start space-y-4 md:space-y-0 flex-row mt-4">
+                {/* First Section: Image and Text in one row */}
+                <div className="flex items-center space-x-4">
+                  <Image
+                    src={require("../assets/logo/Logo_A.png")}
+                    alt="Currency Image"
+                    width={46}
+                    height={46}
+                  />
+                  <div className="flex flex-col space-y-2">
+                    <p className="text-xs font-semibold text-[#3F4765] ">
+                      Algorand{" "}
+                    </p>
+                    <p className="text-sm font-normal text-[#3F4765]">Buy</p>
+                  </div>
+                </div>
+
+                {/* Second Section: Two text elements column-wise */}
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-semibold text-[#24A959] text-end">
+                    $1,435.72
+                  </p>
+                  <p className="text-xs font-normal text-[#3F4765] text-end">
+                    Today | 16.40
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex items-center space-x-4 justify-between mt-6">
+                <div className="font-sans text-lg sm:text-base md:text-lg font-medium leading-5 text-left text-[#3F4765]">
+                  News
+                </div>
+                <div className="font-sans text-sm sm:text-base md:text-sm font-medium leading-5 text-left text-[#969CCB]">
+                  See All
+                </div>
+              </div>
+              {tableData?.map((item) => {
+                return (
+                  <div className="flex justify-between items-start space-y-4 md:space-y-0 flex-row mt-4">
+                    {/* First Section: Image and Text in one row */}
+                    <div className="flex items-center space-x-4 w-[70%]">
+                      <div className="flex flex-col">
+                        <p className="text-sm font-medium text-[#3F4765] ">
+                          Bitcoin, Ethereum, Crypto News and Price Data
+                        </p>
+                        <p className="text-xs font-light text-[#A2A9C7]">
+                          News Media 1h ago
+                        </p>
+                      </div>
+                    </div>
+
+                    {/* Second Section: Two text elements column-wise */}
+                    <div className="flex flex-col  h-10 w-10 bg-[#C4C4C4] border border-gray-300 rounded-md md:h-16 md:w-16"></div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         ) : null}
       </div>
