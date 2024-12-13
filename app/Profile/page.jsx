21393dc7c1618af09d107c77efbea785fec8c867
nabarun1995 <nabarun.adhikary95@gmail.com>
2024-12-13 15:42:23 +0530

    upldate

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -102,11 +102,11 @@ const UserProfile = () => {
             >
               <ArrowLeft size={24} />
             </button>
-            {isEditing && (
+            {/* {isEditing && (
               <button className="absolute bottom-4 right-4 bg-white text-gray-800 px-3 py-2 rounded-full shadow-md flex items-center z-10 hover:bg-gray-100 transition duration-300">
                 <Camera size={18} className="mr-2" /> Change Cover
               </button>
-            )}
+            )} */}
           </div>
 
           {/* Profile Picture */}
@@ -119,11 +119,11 @@ const UserProfile = () => {
                 height={160}
                 className="rounded-full border-4 border-white shadow-lg"
               />
-              {isEditing && (
+              {/* {isEditing && (
                 <button className="absolute bottom-0 right-0 bg-white text-gray-800 p-2 rounded-full shadow-md flex items-center z-10 hover:bg-gray-100 transition duration-300">
                   <Camera size={18} />
                 </button>
-              )}
+              )} */}
             </div>
           </div>
 
