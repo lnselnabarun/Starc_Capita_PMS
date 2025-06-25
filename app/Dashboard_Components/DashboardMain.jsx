18ce68aab66c0c7546b80d1bf56a49b9d40e3d61
nabarun1995 <nabarun.adhikary95@gmail.com>
2025-06-25 14:10:08 +0530

    bug fix

--- app/Dashboard_Components/DashboardMain.jsx
+++ app/Dashboard_Components/DashboardMain.jsx
@@ -1361,7 +1361,7 @@ export default function DashboardMain() {
                   {searchResults.length > 0 && (
                     <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-40 overflow-y-auto">
                       {searchResults.map((result, index) => (
-                        <div>
+                        <>
                           {result?.FSCBI_ISIN !== null ? (
                             <div
                               key={index}
@@ -1379,7 +1379,7 @@ export default function DashboardMain() {
                               {result.FSCBI_LegalName}
                             </div>
                           ) : null}
-                        </div>
+                        </>
                       ))}
                     </div>
                   )}
